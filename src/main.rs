79e193bb7e165a9e29c0daf7bd66e1cb02b30c3b
久保田禅 <zen510510@gmail.com>
2025-11-22 15:47:47 +0900

    2025-11-22

--- src/main.rs
+++ src/main.rs
@@ -3,10 +3,19 @@
 
 use core::panic::PanicInfo;
 
+static HELLO: &[u8] = b"Hello World!";
+
 #[unsafe(no_mangle)] // この関数の名前修飾をしない
 pub extern "C" fn _start() -> ! {
-    // リンカはデフォルトで `_start` という名前の関数を探すので、
-    // この関数がエントリポイントとなる
+    let vga_buffer = 0xb8000 as *mut u8;
+
+    for (i, &byte) in HELLO.iter().enumerate() {
+        unsafe {
+            *vga_buffer.offset(i as isize * 2) = byte;
+            *vga_buffer.offset(i as isize * 2 + 1) = 0xb;
+        }
+    }
+
     loop {}
 }
 
