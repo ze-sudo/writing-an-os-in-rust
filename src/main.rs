d47a75588411a5c456bd5baa363e5e95f4098123
久保田禅 <zen510510@gmail.com>
2025-12-07 19:35:02 +0900

    2025-12-7

--- src/main.rs
+++ src/main.rs
@@ -1,26 +1,35 @@
 #![no_std] // Rust の標準ライブラリにリンクしない
 #![no_main] // 全ての Rust レベルのエントリポイントを無効にする
 
+mod vga_buffer;
+
 use core::panic::PanicInfo;
 
-static HELLO: &[u8] = b"Hello World!";
+// static HELLO: &[u8] = b"Hello World!";
 
-#[unsafe(no_mangle)] // この関数の名前修飾をしない
-pub extern "C" fn _start() -> ! {
-    let vga_buffer = 0xb8000 as *mut u8;
+// #[unsafe(no_mangle)] // この関数の名前修飾をしない
+// pub extern "C" fn _start() -> ! {
+//     let vga_buffer = 0xb8000 as *mut u8;
 
-    for (i, &byte) in HELLO.iter().enumerate() {
-        unsafe {
-            *vga_buffer.offset(i as isize * 2) = byte;
-            *vga_buffer.offset(i as isize * 2 + 1) = 0xb;
-        }
-    }
+//     for (i, &byte) in HELLO.iter().enumerate() {
+//         unsafe {
+//             *vga_buffer.offset(i as isize * 2) = byte;
+//             *vga_buffer.offset(i as isize * 2 + 1) = 0xb;
+//         }
+//     }
 
-    loop {}
-}
+//     loop {}
+// }
 
 /// この関数はパニック時に呼ばれる
 #[panic_handler]
 fn panic(_info: &PanicInfo) -> ! {
+    loop {}
+}
+
+#[unsafe(no_mangle)]
+pub extern "C" fn _start() -> ! {
+    vga_buffer::print_something();
+
     loop {}
 }
